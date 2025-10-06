3d050a315edb5136e6ca1314ac705e1c1d1e4bcb
Sudip-Magar <sudeepmagar730@gmail.com>
2025-10-06 17:22:23 +0545

    sales part completed

---
+++ app/Livewire/User/SaleReturn.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User;
+
+use Livewire\Component;
+
+class SaleReturn extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.sale-return');
+    }
+}
